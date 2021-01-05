34c991828df945ada9aecdad9f5ffecdea57077a
albrtstr <albertusbayu04@gmail.com>
2021-01-05 22:37:43 +0700

    KingdomAnimalia

---
+++ app/src/main/java/com/example/kingdomanimalia/menu_kdi.java
@@ -0,0 +1,14 @@
+package com.example.kingdomanimalia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class menu_kdi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_kdi);
+    }
+}
