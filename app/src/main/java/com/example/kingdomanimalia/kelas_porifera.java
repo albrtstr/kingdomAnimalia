8ae302edafc3455d5c49b06ce73a219272ac2d60
albrtstr <albertusbayu04@gmail.com>
2021-01-08 00:06:47 +0700

    KingdomAnimalia

---
+++ app/src/main/java/com/example/kingdomanimalia/kelas_porifera.java
@@ -0,0 +1,14 @@
+package com.example.kingdomanimalia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class kelas_porifera extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kelas_porifera);
+    }
+}
