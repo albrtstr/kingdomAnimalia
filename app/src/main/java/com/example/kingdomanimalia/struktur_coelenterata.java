78db1197dfc2c0aac8e4519c5de9c5fb06e6250f
albrtstr <albertusbayu04@gmail.com>
2021-01-08 15:11:19 +0700

    KingdomAnimalia

---
+++ app/src/main/java/com/example/kingdomanimalia/struktur_coelenterata.java
@@ -0,0 +1,14 @@
+package com.example.kingdomanimalia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class struktur_coelenterata extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_struktur_coelenterata);
+    }
+}
