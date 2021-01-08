59832e9ba9d66a7b5a5aca035a58c8c9f46427f5
albrtstr <albertusbayu04@gmail.com>
2021-01-08 22:38:50 +0700

    KingdomAnimalia

---
+++ app/src/main/java/com/example/kingdomanimalia/struktur_platyhelminthes.java
@@ -0,0 +1,14 @@
+package com.example.kingdomanimalia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class struktur_platyhelminthes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_struktur_platyhelminthes);
+    }
+}
