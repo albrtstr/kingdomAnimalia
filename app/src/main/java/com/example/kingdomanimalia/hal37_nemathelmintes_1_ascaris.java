28ebc39198c6ba7fedd017c2b2118db293b684c5
Zefanya <vendradevendra@gmail.com>
2021-01-09 23:21:40 +0700

    udah masukin semua component, lanjut ke desainnya

---
+++ app/src/main/java/com/example/kingdomanimalia/hal37_nemathelmintes_1_ascaris.java
@@ -0,0 +1,14 @@
+package com.example.kingdomanimalia;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class hal37_nemathelmintes_1_ascaris extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hal37_nemahelminthes_1_ascaris);
+    }
+}
