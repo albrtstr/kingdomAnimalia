6693ae0c1c4bedac29870b20c68a3c29cbf4f429
Zefanya <vendradevendra@gmail.com>
2021-01-19 19:51:03 +0700

    Update baru navigasi
    kurang yang bagian animasi  obelia, faciola, dan dugesia. Terus masih ada yang gambarnya jelek (Itu diandroid halaman 29 30).

---
+++ app/src/main/java/com/example/kingdomanimalia/hal10_porifera_tipe_ascon.java
@@ -0,0 +1,14 @@
+package com.example.kingdomanimalia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class hal10_porifera_tipe_ascon extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hal10_porifera_tipe_ascon);
+    }
+}
---
+++ app/src/main/java/com/example/kingdomanimalia/hal11_porifera_tipe_sycon.java
@@ -0,0 +1,14 @@
+package com.example.kingdomanimalia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class hal11_porifera_tipe_sycon extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hal11_porifera_tipe_sycon);
+    }
+}
